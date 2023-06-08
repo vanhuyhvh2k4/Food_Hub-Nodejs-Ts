de403efb87a908a50bec6688e9d84cad9a94b138
anhhuydepzai <anhhuydepzai@gmail.com>
2023-06-08 21:44:05 +0700

    filter

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -33,27 +33,66 @@ class homeController {
     }
 
     //[GET] baseURL/home/shop
-    getShop(req: any, res: any) {
+    async getShop(req: any, res: any) {
         try {
+            
             const userId: number = req.user.id;
+            let response:any;
+            const type: string = req.query.type || null;
+            interface options {
+                foodType: string,
+                place: string,
+                rating: number,
+                shipFee: number
+            }
+            
+            const options = {
+                foodType: req.query.foodType || null,
+                place: req.query.place || null,
+                rating: req.query.rating || null,
+            }
+            const queryGetAll = `SELECT COUNT(review.id) AS num_reviews, ROUND(AVG(review.rating), 1) AS avgRating, food_category.name AS foodType, shop.id, shop.name, shop.image, shop.place, shop.isTick, shop.shipFee, IF(shop_like.id IS NULL, 0, 1) as liked FROM shop  JOIN food_item ON shop.id = food_item.shopId LEFT JOIN food_order ON food_item.id = food_order.foodId JOIN food_category ON food_category.id = food_item.foodCategoryId LEFT JOIN review ON food_order.id = review.orderId  LEFT JOIN shop_like ON shop_like.shopId = shop.id AND shop_like.userId = ? GROUP BY shop.id ORDER BY num_reviews DESC, avgRating DESC ${type !== 'all' ? `LIMIT 5` : ``}`;
+            
+            let result: any = await db.promise().query(queryGetAll, ([1]));
 
-            db.query('SELECT COUNT(review.id) AS num_reviews, ROUND(AVG(review.rating), 1) AS avgRating, shop.id, shop.name, shop.image, shop.place, shop.isTick, shop.shipFee, IF(shop_like.id IS NULL, 0, 1) as liked FROM shop  JOIN food_item ON shop.id = food_item.shopId LEFT JOIN food_order ON food_item.id = food_order.foodId LEFT JOIN review ON food_order.id = review.orderId  LEFT JOIN shop_like ON shop_like.shopId = shop.id AND shop_like.userId = ? GROUP BY shop.id ORDER BY num_reviews DESC, avgRating DESC LIMIT 5', ([userId]), (err: any, result: any) => {
-                if (err) throw err;
-                if (result.length) {
-                    res.status(200).json({
-                        code: 'home/getShop.success',
-                        message: 'Success',
-                        data: {
-                            shopList: result
-                        }
-                    });
-                } else {
-                    res.status(404).json({
-                        code: 'home/getShop.notFound',
-                        message: 'Dont found the shop',
-                    });
-                }
-            })
+            //pagination varriables
+            const pageIndex: number = req.query.page || null;
+            let itemPerPage: number = 4;
+            let start = itemPerPage * (pageIndex - 1);
+            let end = start + itemPerPage;
+
+            if (type === 'all' && pageIndex > 0) {
+                    if (options.foodType || options.place || options.rating) {
+                        const filteredData = result[0].filter((item: any) => {
+                            return Object.entries(options)
+                              .filter(([key, value]) => value !== null)
+                              .reduce((acc, [key, value]) => {
+                                return acc && item[key] === value;
+                              }, true);
+                          });
+
+                          response = filteredData.slice(start, end);
+                    } else {
+                        response = result[0].slice(start, end);
+                    }
+            } else {
+                response = result[0];
+            }
+
+            if (response.length) {
+                res.status(200).json({
+                    code: 'home/getShop.success',
+                    message: 'Success',
+                    data: {
+                        shopList: response
+                    }
+                })
+            } else {
+                res.status(404).json({
+                    code: 'home/getShop.notFound',
+                    message: 'Not Found'
+                })
+            }
         } catch (error: any) {
             res.status(500).json({
                 code: 'home/getUser.error',
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,29 @@
-import express from 'express';
+// import express from 'express';
+const express = require('express');
+const app = express();
+const http = require("http").Server(app);
+const io = require("socket.io")(http, {
+    cors: {
+      origin: "http://localhost:8080",
+      methods: ["GET", "POST", "PUT", "PATCH", "DELELTE"]
+    }
+});
 import cors from 'cors';
 import routes from './routes/index';
 import firebaseConfig from './config/firebase.config';
 import {initializeApp} from "firebase/app";
 
-const app: any = express();
+// const app: any = express();
 const port: number = 3000;
-
 let corsOptions: object = {
     origin: 'http://localhost:8080',
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
+
+io.on('connection', (socket: any) => {
+    socket.send('Hello from sever');
+    
+});
 
 //initialize firebase
 initializeApp(firebaseConfig);
@@ -27,6 +40,6 @@ app.use(express.json());
 routes(app);
 
 //initialize port
-app.listen(port, () => {
+http.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
